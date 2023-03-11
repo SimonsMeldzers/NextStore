028094970d607522fab417d429c2c90b2318fbc4
Simon <fiewind1@gmail.com>
2023-03-11 18:48:21 +0200

    Added banner, and best sellers

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,38 @@
 import React from 'react'
 
-const index = () => {
+import { client } from '../lib/client'
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const index = ({ products, bannerData }) => {
   return (
-    <>
-    HeroBanner
     <div>
+    <HeroBanner heroBanner = {bannerData.length && bannerData[0]}/>
+
+    <div className='products-heading'>
       <h2>Best Selling Products</h2>
       <p>Unique phone case for everyone!</p>
     </div>
-    <div>
-      {['Product 1', 'Product 2'].map(
-        (product) => product + ' ')}
+    <div className='products-container'>
+      {products?.map(
+        (product) => <Product key={product._id} product={product} />)}
     </div>
-    Footer
-    </>
+
+    <FooterBanner/>
+    </div>
   )
+}
+
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const BannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(BannerQuery);  
+
+  return {
+    props: { products, bannerData}
+  }
 }
 
 export default index;
